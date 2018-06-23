d310bfa027535eb66ed00e08dac332feea5618a1
Enrico De Guidi <enrico.deguidi@threadsolutions.com>
2018-06-23 10:46:41 +0200

    read input

--- TriangleShortestPath/src/main/java/suprnation/enrico/deguidi/TriangleShortestPath/App.java
+++ TriangleShortestPath/src/main/java/suprnation/enrico/deguidi/TriangleShortestPath/App.java
@@ -1,13 +1,36 @@
 package suprnation.enrico.deguidi.TriangleShortestPath;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args )    {
+        System.out.println( "START" );
+        
+        BufferedReader br = null;
+        try {
+        	br= new BufferedReader(new InputStreamReader(System.in));
+        	String name;
+        	while ((name = br.readLine()) != null) {
+
+        		System.out.println( name );
+        	}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+	        System.out.println( "ERROR" );
+			e.printStackTrace();
+		}
+
+        
+        System.out.println( "END" );
+
     }
+    
+    
 }
